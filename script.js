15b220e0e22849d68f889905d9de88a50f08cb8b
sahar-Nosrati <sahar.nosrati.shanjani@gmail.com>
2025-02-11 07:30:56 +0100

    k6 automation test

---
+++ script.js
@@ -0,0 +1,15 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "5min", target: 50 },
+    { duration: "20min", target: 100 },
+    { duration: "5min", target: 0 },
+  ],
+};
+
+export default function () {
+  http.get("https://k6.io");
+  sleep(1);
+}
